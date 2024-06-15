e418dfe113364331d3a447cc2c22097898c229d6
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-06-15 16:05:27 +0200

    feat: minor changes

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initActions, initControllers } from "@newlogic-digital/utils-js"
+
+export const initStimulus = (element, { controllers, actions }) => {
+    initControllers(element, controllers)
+    initActions(element, actions)
+}
+
+export const useController = (controller, target, application) => {
+    const getController = application.getControllerForElementAndIdentifier(document.querySelector(target || `.${controller}`), controller)
+
+    getController.invoke = (action, event) => getController[action](event)
+
+    return getController
+}
+
+export default { initStimulus, useController }
