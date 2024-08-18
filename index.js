da7b5f80564e3a907652dc11bff97496f54cc476
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-08-18 12:25:41 +0200

    feat: useController fixes

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ export const initStimulus = (element, { controllers, actions }) => {
 }
 
 export const useController = (controller, target, application) => {
-    const getController = application.getControllerForElementAndIdentifier(document.querySelector(target || `.${controller}`), controller)
+    const getController = application.getControllerForElementAndIdentifier(document.querySelector(target || `.${controller}`), controller) ?? {}
 
-    getController.invoke = (action, event) => getController[action](event)
+    getController.invoke = (action, event) => {
+        return getController[action] ? getController[action](event ?? { params: {} }) : undefined
+    }
 
     return getController
 }
