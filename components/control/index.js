bd089bb8af88ce1e3770260b3b3cb571d0663e6b
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-10-23 13:16:16 +0200

    feat: deps update

--- components/control/index.js
+++ components/control/index.js
@@ -1,15 +1,15 @@
 import { Controller } from '@hotwired/stimulus'
-import { dispatchEvent } from '@newlogic-digital/utils-js'
+import { dispatchCustomEvent } from '@newlogic-digital/utils-js'
 
 export class Control extends Controller {
     stepUp() {
         this.element.querySelector('input').stepUp()
-        dispatchEvent(this.element.querySelector('input'))
+        dispatchCustomEvent(this.element.querySelector('input'))
     }
 
     stepDown() {
         this.element.querySelector('input').stepDown()
-        dispatchEvent(this.element.querySelector('input'))
+        dispatchCustomEvent(this.element.querySelector('input'))
     }
 
     showPicker() {
