2a6315baf3b07ac8b1722eb34e39a57205327e88
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-03-06 13:50:15 +0100

    feat: added image controller, updated deps

---
+++ components/image/index.js
@@ -0,0 +1,13 @@
+import { Controller } from '@hotwired/stimulus'
+
+export class Image extends Controller {
+    connect() {
+        const element = this.element.querySelector('img, video, iframe')
+
+        if (!element) return
+
+        if (element?.complete) this.element.classList.remove('before:skeleton')
+        else if (element?.oncanplay) element.oncanplay = () => this.element.classList.remove('before:skeleton')
+        else if (element?.onload) element.onload = () => this.element.classList.remove('before:skeleton')
+    }
+}
