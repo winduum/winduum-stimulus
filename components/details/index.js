e61b03e5803771353fa58dc6226f6b2563425623
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-02 10:23:23 +0200

    feat(version): added sibling details closing and connect method

--- components/details/index.js
+++ components/details/index.js
@@ -16,6 +16,23 @@ export class Details extends Controller {
   async toggle({ currentTarget, params }) {
     const { toggleDetails } = await import('winduum/src/components/details/index.js')
 
+    this.closeSiblings({ params })
+
     await toggleDetails(currentTarget, params)
+  }
+
+  closeSiblings({ params }) {
+    if (!this.element.dataset.name) return
+
+    document.querySelectorAll(`details[data-name="${this.element.dataset.name}"]`).forEach((currentTarget) => {
+      if (currentTarget !== this.element) this.close({ currentTarget, params })
+    })
+  }
+
+  connect() {
+    if (this.element.name) {
+      this.element.dataset.name = this.element.name
+      this.element.removeAttribute('name')
+    }
   }
 }
