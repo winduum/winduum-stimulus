e6f2fa186095994b9887a633406f43fbf1c85741
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-07-08 16:48:40 +0200

    feat(popover): add custom commands and improve popover lifecycle management

--- components/popover/index.js
+++ components/popover/index.js
@@ -10,39 +10,49 @@ export class Popover extends Controller {
   open = false
 
   connect() {
+    this.showPopover = HTMLElement.prototype.showPopover
+    this.hidePopover = HTMLElement.prototype.hidePopover
     this.abortController = new AbortController()
 
     this.element.addEventListener('toggle', (event) => {
       this.open = event.newState === 'open'
       if (this.source?.ariaExpanded) this.source.ariaExpanded = this.open
     }, { signal: this.abortController.signal })
+
+    this.element.addEventListener('command', (e) => {
+      e.preventDefault()
+
+      const method = e.command.replace(/-\w/g, c => c[1].toUpperCase())
+
+      if (method in this.element) this.element[method](e)
+    })
+
+    this.element.showPopover = async ({ source }) => {
+      if ((this.autoUpdateValue && !supportsAnchoredContainer) || !supportsAnchor) {
+        const { autoUpdatePopover } = await import('winduum/src/components/popover/index.js')
+
+        this.cleanup = await autoUpdatePopover(source, this.element, this.placementValue, this.autoUpdateValue)
+      }
+
+      this.source = source
+
+      this.showPopover.call(this.element, { source })
+    }
+
+    this.element.togglePopover = ({ source }) => {
+      !this.open
+        ? this.element.showPopover({ source })
+        : this.element.hidePopover()
+    }
+
+    this.element.hidePopover = () => {
+      this.cleanup?.()
+
+      this.hidePopover.call(this.element)
+    }
   }
 
   disconnect() {
     this.abortController?.abort()
-  }
-
-  async show({ currentTarget }) {
-    if ((this.autoUpdateValue && !supportsAnchoredContainer) || !supportsAnchor) {
-      const { autoUpdatePopover } = await import('winduum/src/components/popover/index.js')
-
-      this.cleanup = await autoUpdatePopover(currentTarget, this.element, this.placementValue, this.autoUpdateValue)
-    }
-
-    this.source = currentTarget
-
-    this.element.showPopover({ source: currentTarget })
-  }
-
-  toggle({ currentTarget }) {
-    !this.open
-      ? this.element.showPopover({ source: currentTarget })
-      : this.element.hidePopover()
-  }
-
-  hide() {
-    this.cleanup?.()
-
-    this.element.hidePopover()
   }
 }
